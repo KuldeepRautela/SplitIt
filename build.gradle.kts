ad68648d2f67af791bb5102df5c1612a2aef8b33
unknown <kuldeeprautela999@gmail.com>
2024-09-15 18:40:42 +0530

    Added Postgresql , implemented login and signUp

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:2.3.12")
     implementation("io.ktor:ktor-server-netty:2.3.12")
     implementation("io.ktor:ktor-server-core:2.3.12")
+    implementation("org.postgresql:postgresql:42.3.1")
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.12")
     implementation ("io.ktor:ktor-serialization-jackson:2.3.12")
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
-
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
@@ -11,42 +10,67 @@ import io.ktor.server.routing.*
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.slf4j.Logger
 
-fun main(){
-    embeddedServer(Netty, port = 8081, watchPaths = listOf("ktorServer"), module = Application::myApp).start(wait = true)
+fun main() {
+    embeddedServer(
+        Netty,
+        port = 8081,
+        watchPaths = listOf("ktorServer"),
+        module = Application::myApp
+    ).start(wait = true)
 
 }
 
-fun Application.myApp(){
-      install(ContentNegotiation){
-          jackson {  }
-      }
-    val database =  Database.connect(
-        url = "jdbc:postgresql://localhost:5432/your-database-name",
+
+object UserInfo : Table() {
+    val username = varchar("username", 50)
+    val password = varchar("password", 30)
+    val id = integer("user_id").autoIncrement()
+    override val primaryKey = PrimaryKey(id, name = "user_id")
+}
+
+fun Application.myApp() {
+    install(ContentNegotiation) {
+        jackson { }
+    }
+    Database.connect(
+        url = "jdbc:postgresql://localhost:5432/SplitIt",
         driver = "org.postgresql.Driver",
-        user = "your-username",
-        password = "your-password"
+        user = "postgres",
+        password = "pswd"
     )
     transaction {
-         SchemaUtils.create(UserInfo)
+        SchemaUtils.create(UserInfo)
     }
     routing {
-        post("/user/signUp"){
+        get("/first") {
+            call.respondText("Server Runnin")
+        }
+        post("/user/signUp") {
             val user = call.receive<User>()
-            if(!user.validate())
-                call.respond(HttpStatusCode.BadRequest,ErrorResponse(HttpStatusCode.BadRequest.value,"Username Or Password length should be more than 5 letters!"))
+            if (!user.validate())
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorResponse(
+                        HttpStatusCode.BadRequest.value,
+                        "Username Or Password length should be more than 5 letters!"
+                    )
+                )
             val userId = transaction {
                 UserInfo.insert {
                     it[username] = user.username
                     it[password] = user.password
                 } get UserInfo.id
             }
-            call.respond(HttpStatusCode.OK,Response(responseCode = HttpStatusCode.OK.value, msg = "User Created Successfully Userid : $userId"))
+            call.respond(
+                HttpStatusCode.OK,
+                Response(responseCode = HttpStatusCode.OK.value, msg = "User Created Successfully Userid : $userId")
+            )
         }
+
         post("/user/login") {
             val user = call.receive<User>()
-            if(!user.validate()) {
+            if (!user.validate()) {
                 call.respond(
                     HttpStatusCode.BadRequest,
                     ErrorResponse(
@@ -58,10 +82,15 @@ fun Application.myApp(){
             }
             runBlocking {
                 val users = transaction {
-                    UserInfo.selectAll().map { it.toUser() }
+                    UserInfo.selectAll().map { it.toUser() }.filter {
+                        it.username == user.username && it.password == user.password
+                    }
                 }
-                if(users.isEmpty())
-                    call.respond(HttpStatusCode.BadRequest,ErrorResponse(HttpStatusCode.BadRequest.value,"You are not registered."))
+                if (users.isEmpty())
+                    call.respond(
+                        HttpStatusCode.BadRequest,
+                        ErrorResponse(HttpStatusCode.BadRequest.value, "You are not registered.")
+                    )
                 else {
                     call.respond(HttpStatusCode.OK, Response(HttpStatusCode.OK.value, "Login Successfully ${users[0]}"))
                 }
@@ -69,17 +98,20 @@ fun Application.myApp(){
 
 
         }
+        post("/user/createGroup") {
+            val groupName = call.request.queryParameters["groupName"]
+            transaction {
+
+            }
+        }
     }
 }
-data class User(val username:String,val password:String){
-fun validate() = !(username.length < 5 || password.length < 5)
+data class User(val username: String, val password: String) {
+    fun validate() = !(username.length < 5 || password.length < 5)
 }
-object UserInfo : Table(){
-    val username = varchar("username",50)
-    val password = varchar("password",30)
-    val id = integer("user_id").autoIncrement()
-    override val primaryKey = PrimaryKey(id, name = "user_id")
-}
-data class ErrorResponse(val responseCode:Int,val msg:String)
-data class Response(val responseCode:Int,val msg:String,val data:Any?=null)
-fun ResultRow.toUser() = User(this[UserInfo.username],this[UserInfo.password])
+data class ErrorResponse(val responseCode: Int, val msg: String)
+data class Response(val responseCode: Int, val msg: String, val data: Any? = null)
+
+fun ResultRow.toUser() = User(this[UserInfo.username], this[UserInfo.password])
+//curl -X POST http://localhost:8081/user/signUp -H "Content-Type: application/json" -d '{"username": "Kanchan Rautela", "password": "password123"}'
+//Invoke-WebRequest -Uri "http://localhost:8081/user/signUp" -ContentType "application/json" -Method POST -Body "{"username": "Kanchan Rautela", "password": "password123"}"
